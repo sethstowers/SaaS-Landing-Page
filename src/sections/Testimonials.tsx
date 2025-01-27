6419df1489e6417e12917708ad8999a5eae8d12d
Seth <stowers3099@gmail.com>
2025-01-27 14:36:01 -0600

    fixed key props on testimonials

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -94,8 +94,8 @@ const TestimonialsColumn = (props: {
     >
       {[...new Array(2)].fill(0).map((_, index) => (
         <React.Fragment key={index}>
-          {props.testimonials.map(({ text, imageSrc, name, username }) => (
-            <div className="card">
+          {props.testimonials.map(({ text, imageSrc, name, username }, index) => (
+            <div className="card" key={index}>
               <div>{text}</div>
               <div className="flex items-center gap-2 mt-5">
                 <Image
